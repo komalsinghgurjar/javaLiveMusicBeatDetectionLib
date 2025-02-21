f52d642f939b035da1f49faff759fd1cca6ddb42
Komal Singh <komalsin9h@gmail.com>
2025-02-21 13:27:51 +0530

    updatr

---
+++ lib/src/main/java/gurjar/singh/komal/javaLiveMusicBeatDetectionLib/SampledAudio.java
@@ -0,0 +1,76 @@
+package gurjar.singh.komal.javaLiveMusicBeatDetectionLib;
+
+import java.util.Arrays;
+
+// Sampled Audio Data Container Class PCM audio data
+public class SampledAudio implements SampledAudioInterface {
+	
+    private int[] sample;        // Array of audio samples
+    private int sampleRate;      // Sampling rate in Hz (e.g., 44100 for standard audio)
+    private int bitDepth;        // Bit depth (e.g., 16 for CD-quality audio)
+    private int sampleCount;     // Number of audio samples
+
+    // Constructor updated to match the new fields
+    public SampledAudio(int[] sample, int sampleRate, int bitDepth) {
+        this.sample = sample;
+        this.sampleRate = sampleRate;
+        this.bitDepth = bitDepth;
+        this.sampleCount = sample.length;
+    }
+
+    @Override
+    public int[] getSamples() {
+        return sample;
+    }
+    
+    @Override
+    public int getBitDepth(){
+    	return bitDepth;
+    }
+
+    @Override
+    public void setSamples(int[] sample) {
+        this.sample = sample;
+        this.sampleCount = sample.length; // Update sampleCount when samples change
+    }
+
+    @Override
+    public int getSampleRate() {
+        return sampleRate;
+    }
+
+    @Override
+    public void setSampleRate(int rate) {
+        this.sampleRate = rate;
+    }
+
+    @Override
+    public int getSampleCount() {
+        return sampleCount;
+    }
+
+    @Override
+    public double getMax() {
+        return Arrays.stream(sample).max().orElse(Integer.MIN_VALUE);
+    }
+
+    @Override
+    public double getMin() {
+        return Arrays.stream(sample).min().orElse(Integer.MAX_VALUE);
+    }
+
+    @Override
+    public double getAvg() {
+        return Arrays.stream(sample).average().orElse(Double.NaN);
+    }
+
+    @Override
+    public String toString() {
+        return "SampledAudio{" +
+               "sample=" + Arrays.toString(sample) +
+               ", sampleRate=" + sampleRate +
+               ", bitDepth=" + bitDepth +
+               ", sampleCount=" + sampleCount +
+               '}';
+    }
+}
---
+++ lib/src/main/java/gurjar/singh/komal/javaLiveMusicBeatDetectionLib/SampledAudioInterface.java
@@ -0,0 +1,30 @@
+package gurjar.singh.komal.javaLiveMusicBeatDetectionLib;
+
+public interface SampledAudioInterface {
+    
+    int getBitDepth();
+    
+    // Method to get the samples
+    int[] getSamples();
+
+    // Method to set the samples
+    void setSamples(int[] sample);
+
+    // Method to get the sampling rate
+    int getSampleRate();
+
+    // Method to set the sampling rate
+    void setSampleRate(int rate);
+
+    // Method to get the size (number of samples)
+    int getSampleCount();
+
+    // Method to get the maximum sample value
+    double getMax();
+
+    // Method to get the minimum sample value
+    double getMin();
+
+    // Method to get the average of the sample values
+    double getAvg();
+}
